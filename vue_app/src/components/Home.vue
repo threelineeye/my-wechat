8e224d0b151396fcd5a3d8ce188c106d8e444374
lcspring <48795232+lcspring@users.noreply.github.com>
2019-06-02 15:11:42 +0800

    home login

--- vue_app/src/components/Home.vue
+++ vue_app/src/components/Home.vue
@@ -1,7 +1,48 @@
 <template>
     <div class="page-tabbar">
         <div class="page-wrap">
-            <titlebar></titlebar>
+            <!-- 标题子组件 -->
+            <titlebar 
+            leftTitle="微信"
+            :rightFirstImg="require('@/assets/search.png')"
+            :rightLastImg="require('@/assets/addition.png')"
+            :search="search"
+            :add="add"></titlebar>
+            <!-- 48px占位 -->
+            <div style="height:48px;"></div>
+            <!-- 切换面板 -->
+            <mt-tab-container v-model="selected" class="page-tabbar-container">
+                <mt-tab-container-item id="msg">
+                    <message></message>
+                </mt-tab-container-item>
+            </mt-tab-container>
+            <!-- tabbar列表 -->
+            <mt-tabbar v-model="selected" fixed>
+                <mt-tab-item id="msg" @click.native="changeState(0)">
+                    <tabbar-icon
+                    :selectedImage="require('@/assets/ic_weixin_selected.png')"
+                    :normalImage="require('@/assets/ic_weixin_normal.png')"
+                    :focused="currentIndex[0].isSelect"></tabbar-icon>微信
+                </mt-tab-item>
+                <mt-tab-item id="contacts" @click.native="changeState(1)">
+                    <tabbar-icon
+                    :selectedImage="require('@/assets/ic_contacts_selected.png')"
+                    :normalImage="require('@/assets/ic_contacts_normal.png')"
+                    :focused="currentIndex[1].isSelect"></tabbar-icon>通讯录
+                </mt-tab-item>
+                <mt-tab-item id="find" @click.native="changeState(2)">
+                    <tabbar-icon
+                    :selectedImage="require('@/assets/ic_find_selected.png')"
+                    :normalImage="require('@/assets/ic_find_normal.png')"
+                    :focused="currentIndex[2].isSelect"></tabbar-icon>发现
+                </mt-tab-item>
+                <mt-tab-item id="me" @click.native="changeState(3)">
+                    <tabbar-icon
+                    :selectedImage="require('@/assets/ic_me_selected.png')"
+                    :normalImage="require('@/assets/ic_me_normal.png')"
+                    :focused="currentIndex[3].isSelect"></tabbar-icon>我的
+                </mt-tab-item>
+            </mt-tabbar>
         </div>
     </div>
 </template>
@@ -9,13 +50,41 @@
 <script>
 // 引入子组件
 import titlebar from './common/TitleBar'
+import message from './common/MessageList'
+import tabbarIcon from './common/TabBarIcon'
 
 export default {
     data(){ 
-        return { }
+        return { 
+            // 面板中显示子组件id
+            selected:"msg",
+            // 创建数组保存图片焦点状态
+            currentIndex:[
+                {isSelect:true},
+                {isSelect:false},
+                {isSelect:false},
+                {isSelect:false},
+            ],
+            id:["msg","contacts","find","me"]
+        }
     },
     components:{
         titlebar,
+        message,
+        tabbarIcon,
+    },
+    methods:{
+        search(){},
+        add(){},
+        changeState(i){
+            this.selected = this.id[i];
+            // 对所有的状态变为false
+            for(var j=0;j<this.currentIndex.length;j++){
+                this.currentIndex[j].isSelect = false;
+            }
+            // 给触发事件的状态显示true
+            this.currentIndex[i].isSelect = true;
+        }
     }
 }
 </script>
---
+++ vue_app/src/components/Login.vue
@@ -0,0 +1,77 @@
+<template>
+    <div style="display:flex;justify-content:center;align-items:center;flex-direction:column;margin-top:20px">
+        <mt-field placeholder="用户名/邮箱/手机号" class="mint-cell-wrapper" v-model="uname">
+        </mt-field>
+        <mt-field placeholder="请输入密码" type="password" class="mint-cell-wrapper" v-model="upwd"></mt-field>
+        <mt-button type="primary" @click="login" size="large" style="margin-top:30px;">登录</mt-button>
+    </div>
+</template>
+
+<script>
+import querystring from 'querystring'
+export default {
+    data(){
+        return {
+            uname:"",
+            upwd:""
+        }
+    },
+    methods:{
+        login(){
+            if(!this.uname){
+                this.$messagebox({
+                    title:"",
+                    message:"请输入用户名"
+                })
+            }else if(!this.upwd){
+                this.$messagebox("","请输入密码")
+            }else{
+                var uname = this.uname,
+                upwd = this.upwd;
+                // 正则验证
+                var reg_uname = /^[a-zA-Z0-9]{3,10}$/,
+                reg_upwd = /^\w{6,10}$/;
+                if(!reg_uname.test(uname)){
+                    this.$toast("用户名格式不正确");
+                    return;
+                }
+                if(!reg_upwd.test(upwd)){
+                    this.$toast("密码格式不正确");
+                    return;
+                }
+                // 发送ajax请求
+                var url = "login";
+                var data = {uname,upwd};
+                // {headers:{'Content-Type':'application/x-www-form-urlencoded'}}
+                this.axios({
+                    url,
+                    method:'post',
+                    data,
+                    transformRequest:[function(data){
+                        // let ret = "";
+                        // for (let it in data){
+                        //     ret += encodeURIComponent(it) + '=' +encodeURIComponent(data[it]) + '&'
+                        // }
+                        // return ret
+                        data = querystring.stringify(data);
+                        return data;
+                    }],
+                    headers:{'Content-Type':'application/x-www-form-urlencoded'}
+                }).then(result=>{
+                    var code = result.data.code;
+                    var msg = result.data.msg;
+                    if(code==-1){
+                        this.$toast(msg)
+                    }else{
+                        this.$router.push("/")
+                    }
+                })
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
