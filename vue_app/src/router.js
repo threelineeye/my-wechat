e0bca92edce3648d9b673437ea0f3ea2293df1c5
lcspring <48795232+lcspring@users.noreply.github.com>
2019-06-03 23:59:20 +0800

    Add files via upload

--- vue_app/src/router.js
+++ vue_app/src/router.js
@@ -2,15 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Error from "./components/Error.vue"
 import Home from "./components/Home.vue"
-import Msgs from "./components/common/MessageList.vue"
+import loginLoading from "./components/common/Login_loading"
 import Login from "./components/Login.vue"
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path:'/',component:Home},
-    {path:'/msgs',component:Msgs},
     {path:'/login',component:Login},
+    {path:'/loginbg',component:loginLoading},
 	
     {path:'*',component:Error},
   ]
